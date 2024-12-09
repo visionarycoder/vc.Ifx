90ba070a9246f0f0c443e3d0ca85e378652bef3b
Ivan Jones <JonesIv@WSDOT.WA.GOV>
2024-12-09 11:00:26 -0800

    Delete unused projects

--- src/Ifx.Web.Security/OAuth/OAuthOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace vc.Ifx.Web.Security.OAuth;
-
-public class OAuthOptions : AuthProviderOptions
-{
-    public string AuthorizationEndpoint { get; set; } = string.Empty;
-    public string TokenEndpoint { get; set; } = string.Empty;
-}
--- src/Ifx.Web.Security/Oidc/OpenIdConnectOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace vc.Ifx.Web.Security.Oidc;
-
-public class OpenIdConnectOptions : AuthProviderOptions
-{
-    public string Authority { get; set; } = string.Empty;
-    public string MetadataAddress { get; set; } = string.Empty;
-}
